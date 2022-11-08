8382570953dd38ead8ffec70b8f54bdcd65a9f6f
BilalEjaz <malhibilal@gmail.com>
2022-11-08 16:29:11 +0100

    changes made in the file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
+
+    public String name;
+    public String city;
+
+    public Main() {
+        this.name = name;
+        this.city = city;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("this is a new project");
+
+
     }
 }
+
